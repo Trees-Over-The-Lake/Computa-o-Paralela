5b34959f704db651ea30bcadf4bfe796b06f79e8
Lucas Santiago <lu.santi.oli@gmail.com>
2022-02-19 17:23:46 -0300

    [feat] tarefa 4 paralelizada?

--- Tarefa 04 - Multiplicação paralela de matrizes/src/multiplicacaoMtraizes.c
+++ Tarefa 04 - Multiplicação paralela de matrizes/src/multiplicacaoMtraizes.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <omp.h>
 
 void mm(double* a, double* b, double* c, int width) 
 {
- for (int i = 0; i < width; i++) {
-    for (int j = 0; j < width; j++) {
-      double sum = 0;
-      for (int k = 0; k < width; k++) {
-	    double x = a[i * width + k];
-	    double y = b[k * width + j];
-	    sum += x * y;
+int i, j, k;
+
+#pragma omp parallel private(i, j, k) shared(a, b, c)
+for (i = 0; i < width; i++) {
+    for (j = 0; j < width; j++) {
+    double sum = 0;
+
+      for (k = 0; k < width; k++) {
+        double x = a[i * width + k];
+        double y = b[k * width + j];
+        sum += x * y;
       }
+      
       c[i * width + j] = sum;
     }
   }
@@ -23,19 +29,24 @@ int main()
   double *b = (double*) malloc (width * width * sizeof(double));
   double *c = (double*) malloc (width * width * sizeof(double));
 
-  for(int i = 0; i < width; i++) {
-    for(int j = 0; j < width; j++) {
-      a[i*width+j] = i;
-      b[i*width+j] = j;
-      c[i*width+j] = 0;
+  omp_set_num_threads(2);
+
+  #pragma omp workshare
+  {
+    for(int i = 0; i < width; i++) {
+      for(int j = 0; j < width; j++) {
+        a[i*width+j] = i;
+        b[i*width+j] = j;
+        c[i*width+j] = 0;
+      }
     }
   }
 
   mm(a,b,c,width);
 
-  //  for(int i = 0; i < width; i++) {
-  //  for(int j = 0; j < width; j++) {
-  //    printf("\n c[%d][%d] = %f",i,j,c[i*width+j]);
-  //  }
+  // for(int i = 0; i < width; i++) {
+  //   for(int j = 0; j < width; j++) {
+  //     printf("\n c[%d][%d] = %f",i,j,c[i*width+j]);
+  //   }
   // }
 }
