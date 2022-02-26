f90904679f823f266d3a7f999b362140ae8432fe
Lucas Santiago <lu.santi.oli@gmail.com>
2022-02-26 11:30:09 -0300

    [feat] tempos do parcode adicionados

--- Tarefa 07 - Crivo de Erastótenes/src/sieve.c
+++ Tarefa 07 - Crivo de Erastótenes/src/sieve.c
@@ -1,5 +1,90 @@
 /*
 * Adapted from: http://w...content-available-to-author-only...s.org/sieve-of-eratosthenes
+
+   Aluno: Lucas Santiago de Oliveira
+   Matrícula: 650888
+
+   Tempo sequêncial:
+   real 4.76
+   user 4.67
+   sys 0.07
+
+   real 4.05
+   user 3.97
+   sys 0.07
+
+   real 4.04
+   user 3.97
+   sys 0.05
+
+   real 4.39
+   user 4.31
+   sys 0.06
+
+   real 4.73
+   user 4.65
+   sys 0.06
+
+   real 4.77
+   user 4.67
+   sys 0.08
+
+   real 4.73
+   user 4.63
+   sys 0.08
+
+   real 4.05
+   user 3.96
+   sys 0.07
+
+   real 4.76
+   user 4.67
+   sys 0.08
+
+   Tempo paralelo:
+   real 3.25
+   user 9.16
+   sys 0.61
+
+   real 3.30
+   user 9.25
+   sys 0.63
+
+   real 3.28
+   user 9.07
+   sys 0.68
+
+   real 3.35
+   user 9.12
+   sys 0.61
+
+   real 3.47
+   user 9.16
+   sys 0.69
+
+   real 3.33
+   user 9.08
+   sys 0.65
+
+   real 3.29
+   user 9.14
+   sys 0.63
+
+   real 3.31
+   user 9.03
+   sys 0.66
+
+   real 3.33
+   user 9.11
+   sys 0.69
+
+   real 3.34
+   user 9.15
+   sys 0.66
+
+   real 3.31
+   user 9.17
+   sys 0.66
 */
 
 #include <stdio.h>
